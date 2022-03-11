7fa49d0ab0fb6059acfaa0715cb0d02ddd0e2486
Federico Buinosquy <fbuinosquy1985>
2022-03-11 10:16:07 -0300

    removed start sh files

--- start-professional-chats.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e  
-cat /vault/secrets/* >> ${SECRETS}
-echo ${SECRETS}
-for s in $(cat /vault/secrets/professional-chat | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" ); do
-    export $s
-done
-npm run awsstart
